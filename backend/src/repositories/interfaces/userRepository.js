e8a0ad4a7fc5dc51d02298229288b748e6e1d2cb
mujahid-jasiq <mujahid.s@jasiqlabs.com>
2025-10-19 17:21:59 +0530

    added IUserRepository interface for consistent DB operations

---
+++ backend/src/repositories/interfaces/userRepository.js
@@ -0,0 +1,6 @@
+export default class IUserRepository {
+  async createUser(userData) {}
+  async findUserByEmail(email) {}
+  async findUserById(userId) {}
+  async updateUser(userId, updateData) {}
+}
